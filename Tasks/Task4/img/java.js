e5f267625533b7040247b7104a57e8265a6a791e
Rahma <124284242+D0NG0L@users.noreply.github.com>
2024-05-29 00:54:57 +0300

    Front-End
    phase one

---
+++ Tasks/Task4/img/java.js
@@ -0,0 +1,22 @@
+
+
+const Protfolio = {
+    name: "Rahma Sameh",
+    occupation: "Front End Developer",
+    location: "Egypt",
+    SocialLinks: {
+        github: "https://github.com/RahmaSameh",
+        linkedin: "https://www.linkedin.com/in/rahma-sameh-2a7a2a1a0",
+        FaceBook: "https://www.facebook.com/rahma.fathy.9406?mibextid=ZbWKwL",
+    },
+};
+const Protfolio = {
+    name:
+    occupation: 
+    location: 
+    SocialLinks: {
+        github: 
+        linkedin: 
+        FaceBook: 
+    },
+};
